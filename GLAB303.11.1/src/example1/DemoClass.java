2c70c717b91d2b69373672e518d873a731f20577
nicholasmathias197 <nicholas.mathias@peopleshores.com>
2026-03-03 10:18:26 -0500

    303.11.1

---
+++ GLAB303.11.1/src/example1/DemoClass.java
@@ -0,0 +1,9 @@
+package example1;
+
+public class DemoClass {
+    public <T> void genericMethod(T data){
+
+        System.out.println("Generic Method");
+        System.out.println("Data Passed: " + data);
+    }
+}
---
+++ GLAB303.11.1/src/example1/myRunner.java
@@ -0,0 +1,13 @@
+package example1;
+
+public class myRunner {
+    static void main(String[] args) {
+
+        DemoClass d0bj =  new DemoClass();
+
+        d0bj.genericMethod(25);
+        d0bj.genericMethod("Per Scholas");
+        d0bj.genericMethod("2563.5");
+        d0bj.genericMethod('H');
+    }
+}
