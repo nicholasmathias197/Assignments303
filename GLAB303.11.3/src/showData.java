f18523eba12dc49e28bf4aa6a54197b181f1f267
nicholasmathias197 <nicholas.mathias@peopleshores.com>
2026-03-03 11:21:10 -0500

    11.3

---
+++ GLAB303.11.3/src/showData.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+public class showData {
+    public static void main(String[] args) {
+
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showValue: mybooklist)
+        {
+
+            System.out.println(showValue.getNumber() + " " + showValue.getName() +" "+ showValue.getCategory() +" "+ showValue.getAuthor());
+        }
+    }
+}
